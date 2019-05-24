8bff66a494db4b66bb195f23a7b5563600edf988
nikolay.tsyb <kartius89@gmail.com>
2019-05-24 14:53:26 +0300

    add explanation comments

--- leetCode/src/main/java/tasks/twoSum/TwoSum.java
+++ leetCode/src/main/java/tasks/twoSum/TwoSum.java
@@ -5,6 +5,20 @@ import java.util.Map;
 
 public class TwoSum {
 
+/*
+
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+            Example:
+
+    Given nums = [2, 7, 11, 15], target = 9,
+
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+            return [0, 1].
+
+ */
 
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
@@ -14,7 +28,7 @@ public class TwoSum {
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i]) && map.get(target - nums[i])!=i) {
+            if (map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i) {
                 result[0] = i;
                 result[1] = map.get(target - nums[i]);
                 return result;
